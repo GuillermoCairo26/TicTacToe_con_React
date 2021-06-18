dc1690ef7dcfb886029f1af9e6c277b65991e0b6
GuillermoCairo26 <guillermocairo14@gmail.com>
2021-06-18 05:38:13 +0000

    finalizado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -16,13 +16,23 @@ export function Home() {
 
 	const [player, setPlayer] = useState("X");
 	const [winner, setWinner] = useState("");
+	const [playerone, setPlayerone] = useState("");
+	const [playertwo, setPlayertwo] = useState("");
+	const [playerlist, setPlayerlist] = useState([]);
 
 	useEffect(() => {
 		validarGanador();
-		if (winner != "") {
-			alert("Winner is: " + winner);
+		if (winner == "X") {
+			alert("Winner is: " + playerlist[0]);
+		} else if (winner == "O") {
+			alert("winner is: " + playerlist[1]);
 		}
 	});
+
+	const names = e => {
+		e.preventDefault();
+		setPlayerlist([playerone, playertwo]);
+	};
 
 	function validarGanador() {
 		for (let i = 0; i < game.length; i = i + 3) {
@@ -62,6 +72,7 @@ export function Home() {
 			}
 		}
 	}
+
 	const marcar = i => {
 		let cambioTurno = false;
 		let newGame = game.map((e, index) => {
@@ -78,36 +89,70 @@ export function Home() {
 	};
 
 	return (
-		<div className="container">
-			{/* Titulo principal del juego */}
-			<div className="titulo_principal">
-				<h1>Tres en línea en React.js</h1>
-			</div>
-			{/* Estado del juego */}
-			<div className="estados">
-				<h3>Es el turno de {player}</h3>
-			</div>
-			{/* Botón volver a empezar */}
-			<div>
-				<button className="boton_volver" onClick={setGame}>
-					Start Over
-				</button>
-			</div>
-			{/* Tablero de juego */}
-			<div className="container-fluid content">
-				<div className="row">
-					{game.map((e, i) => {
-						return (
-							<div
-								key={i}
-								className="col-4 tic-box border"
-								onClick={() => {
-									marcar(i);
-								}}>
-								<p>{e}</p>
-							</div>
-						);
-					})}
+		<div>
+			<br></br>
+			<div className="container">
+				{/* Titulo principal del juego */}
+				<div className="titulo_principal">
+					<h1>Tres en línea en React.js</h1>
+				</div>
+				{/* Elegir x o circulo */}
+				<div>
+					<div className="row">
+						<div className="col-12 d-flex mt-3">
+							<form
+								className="form  d-flexjustify-content-center"
+								onSubmit={names}>
+								<div className="form-group mx-sm-3 mb-2">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Player X name"
+										onChange={e =>
+											setPlayerone(e.target.value)
+										}
+										value={playerone}
+									/>
+								</div>
+
+								<div className="form-group mx-sm-3 mb-2">
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Player O name"
+										onChange={e =>
+											setPlayertwo(e.target.value)
+										}
+										value={playertwo}
+									/>
+								</div>
+								<button type="submit" className="btn ">
+									Go!
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+				{/* Estado del juego */}
+				<div className="estados">
+					<h3>Es el turno de {player}</h3>
+				</div>
+				{/* Tablero de juego */}
+				<div className="container-fluid content">
+					<div className="row">
+						{game.map((e, i) => {
+							return (
+								<div
+									key={i}
+									className="col-4 tic-box border"
+									onClick={() => {
+										marcar(i);
+									}}>
+									<p>{e}</p>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</div>
